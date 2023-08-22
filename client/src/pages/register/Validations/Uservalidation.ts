520f1419981aaa59be279cb78f1ff848617b5565
Mohamed Jeridi <jeridi.moha@gmail.com>
2023-08-22 11:31:05 +0100

    Update Uservalidation.ts

--- client/src/pages/register/Validations/Uservalidation.ts
+++ client/src/pages/register/Validations/Uservalidation.ts
@@ -1,28 +1,32 @@
 import * as Yup from "yup";
 
+const allowedDomains = ["gmail.com", "yahoo.com", "hotmail.com"];
+
 export const userSchema = Yup.object().shape({
   username: Yup.string().required(),
   email: Yup.string()
     .email("Invalid email format")
     .required("Email is required")
     .test("valid-domain", "Invalid email domain", (value) => {
-      // Regular expression to match the domain part of the email
-      const domainRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-      return domainRegex.test(value || "");
+      // Extract domain from email
+      const domain = value.split("@")[1];
+
+      // Check if the domain is in the allowed list
+      return allowedDomains.includes(domain);
     }),
   password: Yup.string().min(8, "Password must be at least 8 characters").required("Password is required"),
 });
 
-
 export const userSchemaSignin = Yup.object().shape({
-   email: Yup.string()
-     .email("Invalid email format")
-     .required("Email is required")
-     .test("valid-domain", "Invalid email domain", (value) => {
-       // Regular expression to match the domain part of the email
-       const domainRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-       return domainRegex.test(value || "");
-     }),
-   password: Yup.string().min(8, "Password must be at least 8 characters").required("Password is required"),
- });
- 
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required")
+    .test("valid-domain", "Invalid email domain", (value) => {
+      // Extract domain from email
+      const domain = value.split("@")[1];
+
+      // Check if the domain is in the allowed list
+      return allowedDomains.includes(domain);
+    }),
+  password: Yup.string().min(8, "Password must be at least 8 characters").required("Password is required"),
+});
