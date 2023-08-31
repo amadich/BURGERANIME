a4a44ee2c12eb739d3cd2747bff6b62cca0142da
Mohamed Jeridi <jeridi.moha@gmail.com>
2023-08-31 17:51:04 +0100

    Update GetListAnime.js

--- server/routes/dashboard/GetListAnime.js
+++ server/routes/dashboard/GetListAnime.js
@@ -12,6 +12,17 @@ router.get("/getlistanime", async (req, res) => {
   }
 });
 
+router.get("/getlistanime/reversed", async (req, res) => {
+  try {
+    const listAnime = await Anime.find();
+    const reversedListAnime = listAnime.reverse(); // Reverse the list
+    return res.status(200).json({ success: true, data: reversedListAnime });
+  } catch (error) {
+    console.error("Error fetching reversed anime list:", error);
+    return res.status(500).json({ success: false, error: "An error occurred while fetching the reversed anime list." });
+  }
+});
+
 router.get("/getlistanime/:id", async (req, res) => {
     const { id } = req.params;
     try {
