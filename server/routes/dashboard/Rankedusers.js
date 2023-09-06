6d5ce72908a2d6e12f6faf581ec5d0887b5dd137
Mohamed Jeridi <jeridi.moha@gmail.com>
2023-09-06 22:17:50 +0100

    Update Rankedusers.js

--- server/routes/dashboard/Rankedusers.js
+++ server/routes/dashboard/Rankedusers.js
@@ -42,6 +42,25 @@ router.post("/toadmin" , async (req, res) => {
 
 });
 
+router.post("/tohelper" , async (req, res) => {
+
+  const { id , newRanks  } = req.body;
+  try {
+
+     const user = await Usermodel.findByIdAndUpdate(id, { ranks : newRanks });
+     if (!user) return res.json({message : "User not found"});
+
+
+     return res.json({ message: `User ID: ${id} has been Ranked to Helper.` });
+
+     
+  } catch (error) {
+    console.log(error);
+    return res.json({error})
+  }
+
+});
+
 
 router.post("/todemo" , async (req, res) => {
 
