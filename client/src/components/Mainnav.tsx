7451b55e699e53123e4531635a1015f9b7e94827
Mohamed Jeridi <jeridi.moha@gmail.com>
2023-09-07 10:05:34 +0100

    Update Mainnav.tsx

--- client/src/components/Mainnav.tsx
+++ client/src/components/Mainnav.tsx
@@ -1,4 +1,5 @@
 import SupraMain from "../../public/assets/images/supramain.png";
+import Burgeranime_sh_0 from "../../public/assets/Burgeranime_sh_0.png";
 import "../assets/css/Navmain.css"
 function Mainnav() {
    const moveDown = () => {
@@ -13,9 +14,15 @@ function Mainnav() {
 
                   <div className=" m-28 flex justify-around">
                   <p className="text-white md:w-96 font-mono select-none hidden md:block">
-                  <span className="text-red-500 font-bold text-3xl ">Burger<span className="text-white">Anime</span></span> is a captivating virtual sanctuary where anime enthusiasts embark on an exhilarating journey through a world of vibrant storytelling, boundless creativity, and awe-inspiring artistry. Nestled within its digital realm, this captivating website curates a delectable collection of hand-picked anime titles, serving them up like a gourmet spread of culinary wonders.
+                  <span className="text-orange-500 font-bold text-6xl uppercase  ">Burger<span className="text-white">Anime</span></span> 
+                  <span className="block text-gray-100">
+                     is a captivating virtual sanctuary where anime enthusiasts embark on an exhilarating journey through a world of vibrant storytelling, boundless creativity, and awe-inspiring artistry. Nestled within its digital realm, this captivating website curates a delectable collection of hand-picked anime titles, serving them up like a gourmet spread of culinary wonders.
+                  </span>
                   <br />
-                  <button onClick={moveDown} id="btn_showme">Show me</button>
+                  {/* <button onClick={moveDown} id="btn_showme">Show me</button> */}
+                  <figure onClick={moveDown} className=" duration-500 hover:mt-[-70%] cursor-pointer ">
+                     <img src={Burgeranime_sh_0} alt="Burgeranime" />
+                  </figure>
                   </p>
                   <img src={SupraMain} alt="Supra Anime" draggable={false} className="select-none hidden md:block"  />
                   </div>
