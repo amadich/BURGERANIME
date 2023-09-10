3843e1909a97e5479ac88499f229d792f34132ee
Mohamed Jeridi <jeridi.moha@gmail.com>
2023-09-10 18:33:53 +0100

    Update Changeavatar.js

--- server/routes/Profile/Changeavatar.js
+++ server/routes/Profile/Changeavatar.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const Usermodel = require("../../models/Schemas/UsersSchema"); // Replace "../path/to/UsersSchema" with the actual path to the UsersSchema file
+const jwt = require("jsonwebtoken");
 
 router.post("/profile/changeavatar", async (req, res) => {
-  const { id, avatarID } = req.body;
+  const { id, avatarID , token } = req.body;
    const urlavatar = `https://firebasestorage.googleapis.com/v0/b/burgeranime-4a245.appspot.com/o/avatars%2F${avatarID}?alt=media`;
   try {
+
+
+    // Verify the token and decode its payload
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+
+    const userID = decodedToken.id;
+    
+    
+
+    if (!userID) {
+      return res.json({ message: "Invalid" });
+    }
+
+    if (userID != id) {
+        return res.json({message: "Error Hack" , ok:407})
+    }
+
+
     // Find the user by ID and update the avatar field
     const updatedUser = await Usermodel.findOneAndUpdate(
       { _id: id },
