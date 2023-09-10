05d737fad7f8fa89d4d75c1170b7cd0c391ed550
Mohamed Jeridi <jeridi.moha@gmail.com>
2023-09-10 11:34:37 +0100

    Update Search.tsx

--- client/src/pages/search/Search.tsx
+++ client/src/pages/search/Search.tsx
@@ -21,24 +21,13 @@ interface Anime {
   rating?: number;
   seasonal?: number;
   eps: Episode[];
-  // Add other fields specific to anime data as needed
 }
 
 export default function Search() {
   const SERVER = import.meta.env.VITE_HOSTSERVER;
   const [listAnime, setListAnime] = useState<Anime[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
-
-  const [affichelist , setAfficheList] = useState<boolean>(false);
-
-  useEffect(() => {
-   if (searchQuery) {
-      setAfficheList(true);
-   }
-   else {
-      setAfficheList(false);
-   }
-  },[searchQuery])
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
 
   useEffect(() => {
     axios.get(`${SERVER}/api/dashboard/getlistanime`).then((response) => {
@@ -47,9 +36,24 @@ export default function Search() {
     });
   }, []);
 
-  const filteredAnime = listAnime.filter((anime) =>
-    anime.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  // Function to handle genre selection/deselection
+  const handleGenreSelect = (genre: string) => {
+    if (selectedGenres.includes(genre)) {
+      // Deselect genre
+      setSelectedGenres(selectedGenres.filter((g) => g !== genre));
+    } else {
+      // Select genre
+      setSelectedGenres([...selectedGenres, genre]);
+    }
+  };
+
+  // Filter anime based on search query and selected genres
+  const filteredAnime = listAnime.filter((anime) => {
+    const titleMatches = anime.title.toLowerCase().includes(searchQuery.toLowerCase());
+    const genreMatches =
+      selectedGenres.length === 0 || selectedGenres.every((genre) => anime.genres.includes(genre));
+    return titleMatches && genreMatches;
+  });
 
   return (
     <>
@@ -64,31 +68,51 @@ export default function Search() {
         />
       </div>
 
-      <div 
-      style={ affichelist ? {display: "block"} : {display: "none"}}
-      className="w-full h-full mt-16 p-16 space-y-4 m-auto text-center">
+      {/* Genre selection section */}
+      <div className="text-center mt-4">
+        <h2 className="text-white text-xl font-bold">Filter by Genres:</h2>
+        <div className="flex flex-wrap justify-center space-x-4 space-y-4 mt-5 w-[70%] m-auto ">
+          {listAnime.reduce((genres, anime) => {
+            anime.genres.forEach((genre) => {
+              if (!genres.includes(genre)) {
+                genres.push(genre);
+              }
+            });
+            return genres;
+          }, [] as string[]).map((genre) => (
+            <button
+              key={genre}
+              className={`${
+                selectedGenres.includes(genre)
+                  ? "bg-orange-500 text-white"
+                  : "bg-transparent text-orange-500"
+              } border border-orange-500 px-3 py-1 rounded-full cursor-pointer hover:bg-orange-500 hover:text-white transition-colors`}
+              onClick={() => handleGenreSelect(genre)}
+            >
+              {genre}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      <div className="w-full h-full mt-16 p-16 space-y-4 m-auto text-center">
         <h1 className="text-white text-2xl font-bold">Meilleurs résultats</h1>
 
-        <div className=" sm:inline-grid md:grid-cols-2 gap-4 ">
+        <div className="flex overflow-auto md:inline-grid md:grid-cols-2 gap-4">
           {filteredAnime.map((anime) => (
-            <Link to={`/series/${anime._id}`}>
-                     <div
-                     key={anime._id}
-                     className="w-72 h-56 bg-[#0000004a] cursor-pointer"
-                     >
-                     <div
-                        style={{ backgroundImage: `url(${anime.imageUrl2})` }}
-                        className="h-32 bg-cover bg-center bg-no-repeat duration-500 hover:transition-opacity hover:opacity-25"
-                     ></div>
-                     <div className="p-5">
-                        <h1 className="text-xl text-white font-bold overflow-hidden overflow-ellipsis">
-                           {anime.title}
-                        
-                        </h1>
-                        
-                        {/* Add other details like genres, description, etc. */}
-                     </div>
-                     </div>
+            <Link to={`/series/${anime._id}`} key={anime._id}>
+              <div className="w-72 h-56 bg-[#0000004a] cursor-pointer">
+                <div
+                  style={{ backgroundImage: `url(${anime.imageUrl2})` }}
+                  className="h-32 bg-cover bg-center bg-no-repeat duration-500 hover:transition-opacity hover:opacity-25"
+                ></div>
+                <div className="p-5">
+                  <h1 className="text-xl text-white font-bold overflow-hidden overflow-ellipsis">
+                    {anime.title}
+                  </h1>
+                  {/* Add other details like genres, description, etc. */}
+                </div>
+              </div>
             </Link>
           ))}
         </div>
