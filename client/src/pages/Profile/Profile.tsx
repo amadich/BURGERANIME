ea16ee1152f546e51442132610b86990c890b97c
Mohamed Jeridi <jeridi.moha@gmail.com>
2023-09-12 09:44:50 +0100

    Update Profile.tsx

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,19 +1,21 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Mainfooter from "../../components/MainFooter";
 import MainHeader from "../../components/MainHeader";
+import Crownbtn from "../../../public/assets/icons/crown.png";
 import axios from "axios";
-import { FormEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import jwtDecode from "jwt-decode";
-import { storage } from "../../models/Firebase";
-import { v4 } from "uuid";
-import { ref , uploadBytes } from "firebase/storage";
 
-import { auth } from "../../models/Firebase";
-import { signInWithEmailAndPassword , signOut } from "firebase/auth";
 
 interface DecodedObject {
   id: string; // Change 'String' to 'string' to match TypeScript type conventions
-  avatar: string
+  avatar: string;
+  ranks: {
+    admin: number;
+    helper: number;
+    demo: number;
+    vip: number;
+  };
   otheraboutme: string
   aboutme:string
 }
@@ -27,15 +29,11 @@ export default function Profile() {
   const [decoded, setDecoded] = useState<DecodedObject | null>(null);
 
   // status
-  const [avatar , setAvatar] = useState<File | null>(null);
-  const [isSubmitting , setIsSubmitting] = useState<boolean>(false);
+
   const [isclickupdate , setIsclickupdate] = useState<boolean>(false);
   const [aboutme,setAboutme] = useState<String>("");
 
-  const [disableChangeAvatar , setDisableChangeAvatar] = useState<boolean>(true);
   const [disableChangeInfo , setDisableChangeInfo] = useState<boolean>(true);
-
-  const [messageResavatar , setMessageResavatar] = useState<string>("");
 
   // information
   const [user,setUser] = useState<String>("");
@@ -89,75 +87,9 @@ export default function Profile() {
    .then((response) => {
       alert(response.data.message);
       setIsclickupdate(false);
-      
    })
   }
-  // Submit Changz avatar
   
-  const handupload = async (e: FormEvent) => {
-   e.preventDefault();
-   setMessageResavatar("Loading ...")
-   
-   if (avatar == null || me == false) return;
-   if ((avatar.type !== "image/png") && (avatar.type !== "image/jpeg")) 
-   {
-    alert('Please select a PNG or JPEG image.'); // Show an error message
-    return;
-   }
-
-   setIsSubmitting(true);
-   
-
-   await signInWithEmailAndPassword(auth,"upload@me.me","gitmegit");
-
-   let myUUID = v4();
-   const photoRef = ref(storage, `avatars/${myUUID}`);
-
-   uploadBytes(photoRef,avatar)
-   .then( async  () => {
-    await signOut(auth);
-      if ( token && decoded) {
-         axios.post(`${SERVER}/api/profile/changeavatar`,{id : decoded.id , avatarID : myUUID , token})
-         .then(() => {
-            setIsSubmitting(false)
-            
-            
-            window.location.href = "";
-            
-         }).catch( async (e) => {
-          await signOut(auth);
-          console.log(e);
-          window.location.replace("/");
-        })
-      }
-   }).catch( async (e) => {
-          await signOut(auth);
-          console.log(e);
-          window.location.replace("/");
-  })
-
-
-}
-
-useEffect(() => {
-
-  if (avatar == null) {setDisableChangeAvatar(true)}
-  if (avatar) {
-      
-      
-      
-        if ((avatar.type !== "image/png") && (avatar.type !== "image/jpeg")) 
-        {
-          setDisableChangeAvatar(true);
-        }
-        else {
-          setDisableChangeAvatar(false)
-        }
-   }
-
-   
-  
-} , [avatar]);
 
   useEffect(() => {
       if (aboutme) {
@@ -167,7 +99,6 @@ useEffect(() => {
         setDisableChangeInfo(true);
       }
   },[aboutme])
-  
 
   return (
     <>
@@ -212,7 +143,7 @@ useEffect(() => {
             </div>
 
             <button
-            disabled={isclickupdate || disableChangeInfo}
+            disabled={isclickupdate || disableChangeInfo }
             onClick={handupdateabout}
             style={me ? {display:"block"} : {display: "none"}}
              type="button" 
@@ -220,23 +151,19 @@ useEffect(() => {
                Update Information
             </button>
 
-            <form onSubmit={handupload}>
-                  <div style={me ? {display:"block"} : {display: "none"}} className="space-y-5">
-                  <button
-                   className="btn btn-primary"
-                    disabled={disableChangeAvatar || isSubmitting} >
-                    Change Avatar</button>
-                  <input 
-                      accept=".png, .jpg, .jpeg"
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setAvatar(e.target.files && e.target.files[0])}}
-                      type="file"
-                      className="file-input file-input-bordered file-input-primary w-full max-w-xs ml-1 md:ml-5" />
-
-                    <span className="pl-5 text-green-500 font-mono " > {messageResavatar} </span>
-
-
-                  </div>
-            </form>
+            
+                <button 
+                disabled={ decoded?.ranks.vip == 1 ? false : true }
+                className="  btn btn-accent mt-5  ">
+                  <Link 
+                  className="flex justify-between items-center space-x-4"
+                  to={ decoded?.ranks.vip === 1 ? `/profile/${decoded.id}/changeavatar` : "" } 
+                  >
+                    <p>Change Avatar</p>
+                    <img src={Crownbtn} alt="Crownbtn" width={20} />
+                  </Link>
+                </button>
+           
             
           </div>
         </div>
